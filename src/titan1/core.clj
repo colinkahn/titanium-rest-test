9557afce620397baff2f4e2c28c6d25a9f725d34
Programming <programming@unknown00254b87356a.att.net>
2013-04-13 16:35:38 -0700

    Getting ring json middleware working

--- src/titan1/core.clj
+++ src/titan1/core.clj
@@ -17,7 +17,8 @@
   "Simplies finding a vertex"
   [k v]
   (tg/transact!
-    (tv/find-by-kv k v)))
+    (first 
+      (tv/find-by-kv k v))))
 
 (defn make-test-graph 
   "Make a temporary graph in memory"
@@ -28,6 +29,37 @@
       ; TODO: update to alpha4-SNAPSHOT and change this 
       ; will become `create-property-key`
       :name String {:indexed true :unique-direction :out})
+    (def v1 (tv/create! {:name "Colin"}))
+    (def v2 (tv/create! {:name "Nathaniel"}))
+    (def v3 (tv/create! {:name "Ryan"}))
+    (te/connect! v1 :employee v2)
+    (te/connect! v3 :employee v2)
+    (te/connect! v1 :coworker v3)
+    (te/connect! v2 :boss v1)
+    (te/connect! v2 :boss v3)
+    )) 
+
+(defn get-coworkers [name]
+  (oq [(f-kv :name name)]
+      (q/--> :coworker)
+      q/map
+      q/all-into-maps!))
+
+(defn add-coworker! [existing new]
+  (let [v1 (f-kv :name existing)]
+    (tg/transact!
+      (def v2 (tv/create! {:name new}))
+      (te/connect! v1 :coworker v2))))
+
+(defn make-real-graph 
+  "Make a graph"
+  [dir]
+  (tg/open dir)
+  (tg/transact! 
+    (tt/create-vertex-key
+      ; TODO: update to alpha4-SNAPSHOT and change this 
+      ; will become `create-property-key`
+      :name String {:indexed true})
     (def v1 (tv/create! {:name "Colin"}))
     (def v2 (tv/create! {:name "Nathaniel"}))
     (def v3 (tv/create! {:name "Ryan"}))
--- src/titan1/web.clj
+++ src/titan1/web.clj
@@ -1,17 +1,29 @@
 (ns titan1.web
     (:use compojure.core)
     (:use ring.middleware.json)
-    (:use ring.util.response))
+    (:use ring.util.response)
+    (:require [titan1.core :as t]))
 
 (defroutes handler
   (GET "/" []
     (response {"hello" "carl"}))
 
-  (PUT "/" [name]
-    (response {"hello" name})))
+  (GET "/:name/coworkers" [name]
+    (response
+      (t/get-coworkers name)))
+
+  (PUT "/" {{name :name} :body} 
+      (t/add-coworker! "Colin" name)
+      (response {"added" name}))
+
+  (PUT "/test" {body :body} 
+    (println body)
+    (response body))
+)
 
 (def app
     (-> handler
           wrap-json-response
-          wrap-json-params))
+          (wrap-json-body {:keywords? true})))
 
+(t/make-test-graph)
